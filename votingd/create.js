6d9d7f29850aadd73268d21cdebc817e2143c97c
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-22 13:37:46 -0500

    checking

--- votingd/create.js
+++ votingd/create.js
@@ -4,8 +4,6 @@ const file = readline.createInterface({
   input: fs.createReadStream('voters.csv')
 });
 
-// Store some data in the faculty database
-
 const mongoose = require('mongoose');
 const connect = require('./db');
 const Voters_info = require('./schema');
@@ -17,17 +15,17 @@ file.on('line', function(line) {
   const columns = line.split(',');
   result.push(
     new Voters_info ({
-    first: columns[0],
-    last: columns[1],
+    first_n: columns[0],
+    last_n: columns[1],
     zipcode: Number(columns[2]),
     history: columns[3]})
   )
-})
+});
 
-file.on('close', function(result)) {
+file.on('close', function(result){
   mongoose.connection.dropDatabase()
     .then(result => result.save())
     .then(() => mongoose.connection.close())
     .then(result => console.log("Ready"))
     .catch(error => console.error(error.stack));
-}
+});
