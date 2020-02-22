50ed3831c919bfe003eb56713becab516efc4dd2
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-22 13:35:51 -0500

    checking

---
+++ votingd/create.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const readline = require('readline');
+const file = readline.createInterface({
+  input: fs.createReadStream('voters.csv')
+});
+
+// Store some data in the faculty database
+
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Voters_info = require('./schema');
+
+connect(); // To the database
+
+const result = [];
+file.on('line', function(line) {
+  const columns = line.split(',');
+  result.push(
+    new Voters_info ({
+    first: columns[0],
+    last: columns[1],
+    zipcode: Number(columns[2]),
+    history: columns[3]})
+  )
+})
+
+file.on('close', function(result)) {
+  mongoose.connection.dropDatabase()
+    .then(result => result.save())
+    .then(() => mongoose.connection.close())
+    .then(result => console.log("Ready"))
+    .catch(error => console.error(error.stack));
+}
---
+++ votingd/schema.js
@@ -0,0 +1,20 @@
+//define a plan for a collection
+
+const mongoose = require('mongoose');
+
+//schema for a collection of professors
+const Voters_info = new mongoose.Schema({
+  first_n: String,
+  last_n: String,
+  zipcode: Number,
+  history: String
+
+});
+// speed up queries on all fields
+Voters_info.index({first_n:1});
+Voters_info.index({last_n:1});
+Voters_info.index({zipcode:1});
+Voters_info.index({history:1});
+
+//compile and export this schema
+module.exports = mongoose.model('Voter', Voters);
