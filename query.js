9dbc1c762620c53c7fa185babe7abc0fefab3912
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-22 18:35:24 -0500

    awxaw

--- query.js
+++ query.js
@@ -5,12 +5,10 @@ const connect = require('./db');
 const Voters_info = require('./schema');
 
 connect(); // To the database
-const i = 0;
+
 const queries = [
 
-  if (Voters_info.find().where('zipcode').equals('13617')){
-    i = i + 1;
-  }
+  Voters_info.find().where('zipcode').equals('13617').count()
 ];
 
 
