4189813cb330443a0cc7939a0a085dd2490be6b4
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-23 15:20:41 -0500

    cds

--- schema.js
+++ schema.js
@@ -1,8 +1,8 @@
-//define a plan for a collection
+
 
 const mongoose = require('mongoose');
 
-//schema for a collection of professors
+
 const Voters_info = new mongoose.Schema({
   first_n: String,
   last_n: String,
@@ -10,11 +10,11 @@ const Voters_info = new mongoose.Schema({
   history: [String]
 
 });
-// speed up queries on all fields
+
 Voters_info.index({first_n:1});
 Voters_info.index({last_n:1});
 Voters_info.index({zipcode:1});
 Voters_info.index({history:1});
 
-//compile and export this schema
+
 module.exports = mongoose.model('Voter', Voters_info);
