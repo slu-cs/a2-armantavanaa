60057da3af472464ca66ecc820b11123c4cdabda
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-23 18:56:54 -0500

    sdjkv

--- query.js
+++ query.js
@@ -22,6 +22,6 @@ Promise.all(queries)
     console.log('What are the full names of all the registered voters whose first-name is STARR? ', results[1].map(x=> x.first_n + " " + x.last_n));
     console.log('How many people voted in the 2016 general election (GE16)? ', results[2]);
     console.log('What is the last-name that comes last in the county in alphabetical order? ', results[3]);
-    console.log('How many zip codes does the county contain? ', results[4].length);
+    console.log('How many zip codes does the county contain? ', results[4]);
 
 }).catch(error => console.error(error.stack));
