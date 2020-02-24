6fefda7f53ebabfd1dded4c5d1781192682a2891
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-23 19:09:23 -0500

    biuiu

--- query.js
+++ query.js
@@ -21,7 +21,7 @@ Promise.all(queries)
     console.log('voters in canton: ', results[0]);
     console.log('What are the full names of all the registered voters whose first-name is STARR? ', results[1].map(x=> x.first_n + " " + x.last_n));
     console.log('How many people voted in the 2016 general election (GE16)? ', results[2]);
-    console.log('What is the last-name that comes last in the county in alphabetical order? ', results[3]);
+    console.log('What is the last-name that comes last in the county in alphabetical order? ', results[3].map(x => x.last_n));
     console.log('How many zip codes does the county contain? ', results[4].length);
 
 }).catch(error => console.error(error.stack));
